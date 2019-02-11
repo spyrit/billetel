b968bec6ff252fc4ac3524a3c9509145d4ca9b19
Alexandre Cousin <alexandre.cousin@spyrit.net>
2019-02-11 15:36:35 +0100

    add booking availability client

---
+++ Client/BookingAvailabilityClient.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Spyrit\Billetel\Client;
+
+use Spyrit\Billetel\Client\AbstractClient;
+
+class AvailabilityBookingClient extends AbstractClient
+{
+    /**
+     * @return SimpleXMLElement
+     */
+    public function get($eventId, $sessionId)
+    {
+        $uri = '/bol/api/booking/v2/events/'. $eventId .'/sessions/'. $sessionId .'/availability';
+
+        return $this->action('GET', $uri);
+    }
+}
